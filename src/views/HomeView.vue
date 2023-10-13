364e25c8a038f85952fad15d9655a5b78a5b859b
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-14 01:32:45 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <LogoSlider class="mt-14" />
+    <About />
+    <BestSeller />
+    <PopularCategory />
   </div>
 </template>
 <script>
+import About from "../components/About.vue";
+import BestSeller from "../components/BestSeller.vue";
 import LogoSlider from "../components/LogoSlider.vue";
+import PopularCategory from "../components/PopularCategory.vue";
 
 export default {
   data() {
     return {};
   },
-  components: { LogoSlider },
+  components: { LogoSlider, About, BestSeller, PopularCategory },
 };
 </script>
