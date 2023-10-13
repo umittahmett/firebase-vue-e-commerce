2c20c46a1a3a01db649129b2189dd0dc91fadec4
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-14 01:31:07 +0300

    Create About compoenent

---
+++ src/components/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-[#F4F8FD] py-20 mt-10">
+    <div
+      class="max-w-7xl w-full mx-auto flex justify-center gap-10 lg:gap-20 items-center px-2"
+    >
+      <div class="relative">
+        <img
+          class="h-[380px] lg:h-[450px] object-cover bg-center rounded-lg"
+          src="/hero/about.jpg"
+          alt=""
+        />
+
+        <a
+          class="text-[8px] absolute p-1 bottom-0 left-0 z-10 text-gray-200"
+          href="http://www.freepik.com"
+          >Designed by macrovector / Freepik</a
+        >
+      </div>
+
+      <div>
+        <h2 class="text-blue-500 uppercase font-semibold text-xl">About Us</h2>
+        <h4 class="text-5xl font-medium mt-6">Who We Are</h4>
+        <p class="mt-6 max-w-md">
+          At Seçkin Bilişim, we’re passionate about bringing you the best in
+          quality electronics. Our team is dedicated to providing top-notch
+          customer service and offering a wide range of products to meet your
+          needs. Whether you’re in the market for a new phone, computer, or
+          laptop, we’ve got you covered.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  setup() {
+    return {};
+  },
+};
+</script>
