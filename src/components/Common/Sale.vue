f4360a4cc45978757b20aa7afedbcca6e4839c92
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-14 01:31:46 +0300

    Create Sale component

---
+++ src/components/Common/Sale.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="rounded-full text-white font-bold p-2 flex justify-center bg-blue-500 items-center absolute top-[-10px] right-[-10px]"
+  >
+    indirimli !
+  </div>
+</template>
