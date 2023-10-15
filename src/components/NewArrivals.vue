f97c826050d171ae11c5929c35046ba07f6424d6
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-16 00:35:18 +0300

    Create NewArrivals component

---
+++ src/components/NewArrivals.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="max-w-7xl w-full mx-auto py-10 px-2">
+    <div class="flex justify-between items-center w-full ">
+      <h2 class="text-xl font-medium">New Arrivals</h2>
+      <button
+      class="hover:text-blue-500 text-gray-400 transition-colors hover:border-blue-500 text-xl font-medium border-b border-gray-400 font-light"
+      >
+        View All New Arrivals
+      </button>
+    </div>
+
+    <div class="flex items-center text-center gap-4 gap-y-10 mt-10 lg:flex-row flex-col justify-between w-full">
+      <div v-for="product in products" :key="product.name" class="relative ">
+        <img
+          class="object-cover bg-center w-[400px] h-[340px]"
+          :src="product.image"
+          alt=""
+        />
+        <div class="px-2">
+          <h4 class="font-medium text-xl line-clamp-2 mt-4">
+            {{ product.name }}
+          </h4>
+
+          <p class="text-gray-500 line-clamp-4 mt-2 font-light">
+            {{ product.description }}
+          </p>
+
+          <p class="mt-2 text-gray-700 font-light text-xl flex items-center gap-x-1">
+           <div class="flex items-center gap-1 justify-center w-full">
+            <div v-if="product.discount" class="">
+             <p class="text-gray-600 line-through text-base ">{{ product.price }}&#8378;</p> 
+            </div>
+
+            {{ product.discount ? product.price - product.price/100 * product.discount :product.price }}&#8378;
+           </div>
+          </p>
+        </div>
+        
+        <Sale :bgColor="'black'" v-if="product.discount" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Sale from "../components/Common/Sale.vue";
+
+export default {
+  components: { Sale },
+  data() {
+    return {
+      products: null,
+    };
+  },
+
+  mounted() {
+    this.products = [
+      {
+        name: "Asus",
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        price: 999.99,
+        discount: 20,
+        image: "/example/pc.png",
+      },
+      {
+        name: "Iphone 12s",
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        price: 499.99,
+        discount: 0,
+        image: "/example/pc.png",
+      },
+
+      {
+        name: "MSI",
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        price: 1299.99,
+        discount: 10,
+        image: "/example/pc.png",
+      },
+    ];
+  },
+};
+</script>
