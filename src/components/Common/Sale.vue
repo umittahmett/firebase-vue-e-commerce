b85c51c07f0daacd21567bf166a3a78f244f5b53
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-16 00:36:33 +0300

    Update Sale component

--- src/components/Common/Sale.vue
+++ src/components/Common/Sale.vue
@@ -1,7 +1,22 @@
 <template>
   <div
-    class="rounded-full text-white font-bold p-2 flex justify-center bg-blue-500 items-center absolute top-[-10px] right-[-10px]"
+    class="rounded-full text-white font-bold p-2 flex justify-center items-center absolute top-[-10px] right-[-10px]"
+    :class="{
+      'bg-black': bgColor === 'black',
+      'bg-blue-500': bgColor === 'blue',
+    }"
   >
-    indirimli !
+    indirimli!
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    bgColor: {
+      type: String,
+      default: "blue",
+    },
+  },
+};
+</script>
