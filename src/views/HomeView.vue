77f6665f23ef1ce2478b164810be1aa50266c5cc
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-16 00:37:08 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <LogoSlider class="mt-14" />
-    <About />
-    <BestSeller />
-    <PopularCategory />
+    <LogoSlider />
+    <About class="mt-10" />
+    <BestSeller class="mt-10" />
+    <PopularCategory class="mt-10" />
+    <NewArrivals class="mt-10" />
+    <OurMission id="mission" class="mt-10" />
+    <Features />
+    <Footer />
   </div>
 </template>
+
 <script>
 import About from "../components/About.vue";
 import BestSeller from "../components/BestSeller.vue";
 import LogoSlider from "../components/LogoSlider.vue";
+import NewArrivals from "../components/NewArrivals.vue";
 import PopularCategory from "../components/PopularCategory.vue";
+import OurMission from "../components/OurMission.vue";
+import Features from "../components/Features.vue";
+import Footer from "../components/Footer.vue";
 
 export default {
   data() {
     return {};
   },
-  components: { LogoSlider, About, BestSeller, PopularCategory },
+  components: {
+    LogoSlider,
+    About,
+    BestSeller,
+    PopularCategory,
+    NewArrivals,
+    OurMission,
+    Features,
+    Footer,
+  },
 };
 </script>
