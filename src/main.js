16f55424b375fe2289578030d24d64764856b62d
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-22 14:57:53 +0300

    Create product image galerry

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import { createPinia } from "pinia";
 import { markRaw } from "vue";
 import "./assets/index.css";
+import PrimeVue from "primevue/config";
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -11,6 +12,7 @@ const app = createApp(App);
 pinia.use(({ store }) => {
   store.$router = markRaw(router);
 });
+app.use(PrimeVue);
 app.use(router);
 app.use(pinia);
 
--- src/views/ProductOverview.vue
+++ src/views/ProductOverview.vue
@@ -1,3 +1,93 @@
-<template>Overview</template>
+<template>
+  <div class="max-w-7xl mx-auto py-10">
+    <Galleria
+      :value="images"
+      :responsiveOptions="responsiveOptions"
+      :numVisible="5"
+      :circular="true"
+      containerStyle="max-width: 630px"
+      :showItemNavigators="true"
+    >
+      <template #item="slotProps">
+        <div class="w-[600px] h-[500px] flex justify-center items-center">
+          <img
+            class="w-full bg-cover"
+            :src="slotProps.item.itemImageSrc"
+            :alt="slotProps.item.alt"
+            style="width: 100%; display: block"
+          />
+        </div>
+      </template>
+      <template #thumbnail="slotProps">
+        <img
+          class="w-[130px] h-[100px] bg-center object-cover"
+          :src="slotProps.item.thumbnailImageSrc"
+          :alt="slotProps.item.alt"
+          style="display: block"
+        />
+      </template>
+    </Galleria>
+  </div>
+</template>
 
-<script></script>
+<script>
+import Galleria from "primevue/galleria";
+
+export default {
+  name: "MainSlider",
+  components: {
+    Galleria,
+  },
+  data() {
+    return {
+      images: [
+        {
+          itemImageSrc: "/public/about.png",
+          thumbnailImageSrc: "/public/about.png",
+          alt: "Image 1",
+        },
+        {
+          itemImageSrc: "/public/OurMissionBg.jpg",
+          thumbnailImageSrc: "/public/OurMissionBg.jpg",
+          alt: "Image 2",
+        },
+        {
+          itemImageSrc: "/public/about.png",
+          thumbnailImageSrc: "/public/about.png",
+          alt: "Image 1",
+        },
+        {
+          itemImageSrc: "/public/OurMissionBg.jpg",
+          thumbnailImageSrc: "/public/OurMissionBg.jpg",
+          alt: "Image 2",
+        },
+        {
+          itemImageSrc: "/public/about.png",
+          thumbnailImageSrc: "/public/about.png",
+          alt: "Image 1",
+        },
+        {
+          itemImageSrc: "/public/OurMissionBg.jpg",
+          thumbnailImageSrc: "/public/OurMissionBg.jpg",
+          alt: "Image 2",
+        },
+      ],
+      responsiveOptions: [
+        {
+          breakpoint: "1080px",
+          numVisible: 5,
+        },
+        {
+          breakpoint: "768px",
+          numVisible: 3,
+        },
+        {
+          breakpoint: "560px",
+          numVisible: 1,
+        },
+      ],
+    };
+  },
+  galleriaService: null,
+};
+</script>
