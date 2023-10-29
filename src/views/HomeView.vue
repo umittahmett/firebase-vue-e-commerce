e76a79cddd51afa771a6befe1b4b19b5111f6813
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-29 20:29:43 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,17 @@
   <div>
     <LogoSlider />
     <About class="mt-10" />
-    <BestSeller class="mt-10" />
+    <ProductSlider
+      :name="'Yeni Gelenler'"
+      :products="newArrvals"
+      class="mt-10"
+    />
     <PopularCategory class="mt-10" />
-    <NewArrivals class="mt-10" />
+    <ProductSlider
+      :name="'En Cok Satanlar'"
+      :products="pupolar"
+      class="mt-10"
+    />
     <OurMission id="mission" class="mt-10" />
     <Features />
   </div>
@@ -12,23 +20,161 @@
 
 <script>
 import About from "../components/About.vue";
-import BestSeller from "../components/BestSeller.vue";
+import ProductSlider from "../components/ProductSlider.vue";
 import LogoSlider from "../components/LogoSlider.vue";
-import NewArrivals from "../components/NewArrivals.vue";
 import PopularCategory from "../components/PopularCategory.vue";
 import OurMission from "../components/OurMission.vue";
 import Features from "../components/Features.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      newArrvals: [
+        {
+          name: "Asus",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 999.99,
+          discount: 20,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Iphone 12s",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 499.99,
+          discount: 0,
+          image: "/example/pc.png",
+        },
+
+        {
+          name: "Asus",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 999.99,
+          discount: 20,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Iphone 12s",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 499.99,
+          discount: 0,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Asus",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 999.99,
+          discount: 20,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Iphone 12s",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 499.99,
+          discount: 0,
+          image: "/example/pc.png",
+        },
+
+        {
+          name: "MSI",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 1299.99,
+          discount: 10,
+          image: "/example/pc.png",
+        },
+
+        {
+          name: "MSI",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 1299.99,
+          discount: 10,
+          image: "/example/pc.png",
+        },
+      ],
+
+      pupolar: [
+        {
+          name: "Asus",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 999.99,
+          discount: 20,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Iphone 12s",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 499.99,
+          discount: 0,
+          image: "/example/pc.png",
+        },
+
+        {
+          name: "Asus",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 999.99,
+          discount: 20,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Iphone 12s",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 499.99,
+          discount: 0,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Asus",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 999.99,
+          discount: 20,
+          image: "/example/pc.png",
+        },
+        {
+          name: "Iphone 12s",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 499.99,
+          discount: 0,
+          image: "/example/pc.png",
+        },
+
+        {
+          name: "MSI",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 1299.99,
+          discount: 10,
+          image: "/example/pc.png",
+        },
+
+        {
+          name: "MSI",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+          price: 1299.99,
+          discount: 10,
+          image: "/example/pc.png",
+        },
+      ],
+    };
   },
   components: {
     LogoSlider,
     About,
-    BestSeller,
+    ProductSlider,
     PopularCategory,
-    NewArrivals,
     OurMission,
     Features,
   },
