7f793e37f8c9b4690bd4c267c03473e809212043
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-11-11 15:50:08 +0300

    update store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,8 +1,37 @@
 import { defineStore } from "pinia";
-
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 export const createWizardStore = defineStore("counter", {
   state: () => ({
-    currentProduct: {},
+    categories: [],
+    prices: [{ min: null, max: null }],
   }),
-  actions: {},
+  actions: {
+    async search() {
+      const searchedWord = localStorage.getItem("searchedWord");
+      const db = getFirestore();
+      const productsRef = collection(db, "products");
+
+      const productSnapshot = await getDocs(productsRef);
+
+      if (!productSnapshot.empty) {
+        this.products = productSnapshot.docs
+          .map((doc) => doc.data())
+          .filter((product) => {
+            const { title, description, model } = product;
+            return (
+              title.toLowerCase().includes(searchedWord.toLowerCase()) ||
+              description.toLowerCase().includes(searchedWord.toLowerCase()) ||
+              model.toLowerCase().includes(searchedWord.toLowerCase())
+            );
+          });
+
+        console.log(this.products);
+      } else {
+        this.products = [];
+        console.log("Ürün bulunamadı.");
+      }
+
+      this.$router.push("/search");
+    },
+  },
 });
