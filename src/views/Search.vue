c937e0bb9a2170db8066c938260ae1b746586aed
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-11-11 15:49:37 +0300

    Update Search.vue

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-7xl w-full mx-auto">
+  <div v-if="store" class="max-w-7xl w-full mx-auto">
     <BreadCrumb class="border-b-0 mt-4 mb-4" />
     <div class="flex justify-start items-start gap-4">
       <div class="w-full rounded-md max-w-[200px]">
@@ -102,48 +102,14 @@
         </form>
 
         <!-- Products -->
-        <div class="grid grid-cols-4 justify-center items-start gap-5 mt-4">
-          <div
-            v-for="product in products"
+        <div class="grid grid-cols-4 justify-center items-start gap-2 mt-4">
+          <Product
+            v-for="product in store.products"
             :key="product.name"
+            :product="product"
+            :text_align="'start'"
             class="border rounded-md w-fit cursor-pointer overflow-hidden"
-          >
-            <div class="relative mx-auto">
-              <img
-                class="object-cover bg-center w-[300px] h-[300px] rounded-t-md hover:scale-105 transition-all"
-                :src="'/public/example/img1.webp'"
-                alt=""
-              />
-              <Sale :bgColor="'blue'" v-if="product.discount" />
-            </div>
-            <div class="px-2">
-              <h4 class="font-medium text-xl line-clamp-1 mt-4">
-                {{ product.name }}
-              </h4>
-
-              <p class="text-gray-500 line-clamp-2 mt-2 font-light">
-                {{ product.description }}
-              </p>
-
-              <div
-                class="my-2 text-gray-700 font-light text-xl flex items-start gap-x-1"
-              >
-                <div class="flex items-start gap-1 justify-start w-full">
-                  <div v-if="product.discount" class="">
-                    <p class="text-gray-600 line-through text-base">
-                      {{ product.price }}&#8378;
-                    </p>
-                  </div>
-
-                  {{
-                    product.discount
-                      ? product.price - (product.price / 100) * product.discount
-                      : product.price
-                  }}&#8378;
-                </div>
-              </div>
-            </div>
-          </div>
+          />
         </div>
       </div>
     </div>
@@ -152,6 +118,8 @@
 
 <script>
 import BreadCrumb from "../components/Common/BreadCrumb.vue";
+import Product from "../components/Common/Product.vue";
+import { createWizardStore } from "../stores/counter";
 export default {
   data() {
     return {
@@ -167,79 +135,21 @@ export default {
         { min: 10000, max: 20000 },
         { min: 20000, max: 0 },
       ],
-      products: [
-        {
-          name: "Asus",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 999.99,
-          discount: 20,
-          image: "/example/pc.png",
-        },
-        {
-          name: "Iphone 12s",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 499.99,
-          discount: 0,
-          image: "/example/pc.png",
-        },
-
-        {
-          name: "Asus",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 999.99,
-          discount: 20,
-          image: "/example/pc.png",
-        },
-        {
-          name: "Iphone 12s",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 499.99,
-          discount: 0,
-          image: "/example/pc.png",
-        },
-        {
-          name: "Asus",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 999.99,
-          discount: 20,
-          image: "/example/pc.png",
-        },
-        {
-          name: "Iphone 12s",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 499.99,
-          discount: 0,
-          image: "/example/pc.png",
-        },
-
-        {
-          name: "MSI",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 1299.99,
-          discount: 10,
-          image: "/example/pc.png",
-        },
-
-        {
-          name: "MSI",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-          price: 1299.99,
-          discount: 10,
-          image: "/example/pc.png",
-        },
-      ],
+      store: null,
     };
+  },
+  methods: {
+    search() {
+      localStorage.setItem("searchedWord", this.searchedWord);
+      this.store.search();
+    },
   },
   components: {
     BreadCrumb,
+    Product,
+  },
+  mounted() {
+    this.store = createWizardStore();
   },
 };
 </script>
