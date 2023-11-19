d188620ad32b7e0f7f713d87bad454d12528d5e2
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-11-19 16:07:46 +0300

    Add search function

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,34 +3,113 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 export const createWizardStore = defineStore("counter", {
   state: () => ({
     products: [],
+    loading: false,
+    emptyResult: false,
+    priceRanges: [],
   }),
   actions: {
     async search() {
-      const searchedWord = localStorage.getItem("searchedWord");
+      this.loading = true;
+      this.emptyResult = false;
+      const searchedWordMethod = JSON.parse(
+        localStorage.getItem("searchedWord")
+      );
+      const searchedWord = searchedWordMethod;
+
+      const searchedCategoryMethod = JSON.parse(
+        localStorage.getItem("searchedCategory")
+      );
+      const searchedCategory = searchedCategoryMethod;
       const db = getFirestore();
       const productsRef = collection(db, "products");
 
       const productSnapshot = await getDocs(productsRef);
 
-      if (!productSnapshot.empty) {
+      if (!productSnapshot[0]) {
         this.products = productSnapshot.docs
           .map((doc) => doc.data())
           .filter((product) => {
-            const { title, description, model } = product;
-            return (
-              title.toLowerCase().includes(searchedWord.toLowerCase()) ||
-              description.toLowerCase().includes(searchedWord.toLowerCase()) ||
-              model.toLowerCase().includes(searchedWord.toLowerCase())
-            );
+            const { title, description, model, category_id } = product;
+            if (searchedWord.addingDate > searchedCategory.addingDate) {
+              return (
+                title.toLowerCase().includes(searchedWord.word.toLowerCase()) ||
+                description
+                  .toLowerCase()
+                  .includes(searchedWord.word.toLowerCase()) ||
+                model.toLowerCase().includes(searchedWord.word.toLowerCase())
+              );
+            } else {
+              return category_id == searchedCategory.category_id;
+            }
           });
+        this.loading = false;
 
-        console.log(this.products);
+        if (!this.products[0]) {
+          this.emptyResult = true;
+          console.log("Ürün bulunamadı.");
+          this.loading = false;
+        }
       } else {
-        this.products = [];
         console.log("Ürün bulunamadı.");
+        this.loading = false;
       }
 
-      this.$router.push("/search");
+      const allPrices = this.products.map((product) =>
+        product.discount
+          ? (product.price * product.discount) / 100
+          : product.price
+      );
+
+      const minPrice = allPrices.length > 0 ? Math.min(...allPrices) : 0;
+      const maxPrice = allPrices.length > 0 ? Math.max(...allPrices) : 0;
+      this.priceRanges = [];
+
+      for (let i = 0; i < 5; i++) {
+        const rangeWidth = (maxPrice - minPrice) / 5;
+        const minRange = minPrice + i * rangeWidth;
+        const maxRange = minPrice + (i + 1) * rangeWidth;
+
+        // Fiyat aralığında en az bir ürün var mı kontrol et
+        const hasProductsInRange = this.products.some(
+          (product) =>
+            (product.discount
+              ? (product.price * product.discount) / 100
+              : product.price) >= minRange &&
+            (product.discount
+              ? (product.price * product.discount) / 100
+              : product.price) < maxRange
+        );
+
+        if (hasProductsInRange) {
+          this.priceRanges.push({
+            id: i,
+            min: Math.floor(minRange / 1000) * 1000,
+            max: Math.ceil(maxRange / 1000) * 1000,
+            activity: false,
+          });
+        }
+      }
+    },
+
+    addSearchedCategory(category_id) {
+      const now = Date.now();
+      const data = {
+        category_id: category_id,
+        category_name:
+          category_id == 0
+            ? "Bilgisayar"
+            : category_id == 1
+            ? "Laptop"
+            : category_id == 2
+            ? "Monitör"
+            : category_id == 3
+            ? "Güvenlik Kamerası"
+            : "kategori",
+
+        addingDate: now,
+      };
+
+      localStorage.setItem("searchedCategory", JSON.stringify(data));
     },
   },
 });
