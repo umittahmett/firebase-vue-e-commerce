8dda184c5e72840ade2e041404452e11f71a8856
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-11-21 21:56:09 +0300

    Update Hero.vue

--- src/components/Menu/Hero.vue
+++ src/components/Menu/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-[85vh] overflow-hidden relative text-white">
+  <div class="w-full h-[90vh] overflow-hidden relative text-white">
     <img
       class="object-cover w-full h-full bg-center"
       src="/hero/heroBg.jpg"
@@ -7,21 +7,22 @@
     />
 
     <div
-      class="absolute top-0 left-0 w-full sm:pt-28 pt-20 sm:pl-10 pl-4 h-full bg-[rgba(0,0,0,0.3)]"
+      class="absolute top-0 left-0 w-full sm:pt-24 pt-10 sm:pl-10 pl-4 h-full bg-[rgba(0,0,0,0.3)]"
     >
       <div>
         <h1
           class="text-[55px] leading-[60px] sm:text-7xl font-bold max-w-sm md:max-w-xl"
         >
-          Seçkin Bilişim Shop for <br />
-          Quality Electronics
+          Seçkin Bilişim <br />
+          Profesyonel <br />
+          IT Destek
         </h1>
-        <p class="mt-10 max-w-md text-lg">
-          in this site i will share my products with people. and in this site
-          will be a admin panel for upload new products
+        <p class="mt-6 max-w-md">
+          Seçkin Bilişim Mağazası, güvenilir elektronik ürünleriyle bilgi ve
+          kaliteyi bir araya getiriyor. Kaliteli alışverişin adresi!
         </p>
 
-        <div class="flex items-center justify-start gap-4 mt-10">
+        <div class="flex items-center justify-start gap-4 mt-6">
           <Button :buttonType="'white'">İletişim</Button>
         </div>
       </div>
