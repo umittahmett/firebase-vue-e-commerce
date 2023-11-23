765ea7b624c775e74691e2ee443e928bf8e4565b
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-11-23 23:54:57 +0300

    Update LogoSlider.vue

--- src/components/LogoSlider.vue
+++ src/components/LogoSlider.vue
@@ -21,29 +21,14 @@
         },
 
         1024: {
-          slidesPerView: 4,
+          slidesPerView: 5,
         },
       }"
       :modules="modules"
-      class="mySwiper pb-10 px-4 justify-center flex"
+      class="mySwiper pb-10 px-4 justify-center items-center flex"
     >
-      <swiper-slide
-        ><img
-          class="sm:h-[200px] md:h-[230px] object-cover w-full"
-          src="/Logos/wossew.jpeg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          class="sm:h-[200px] md:h-[230px] object-cover w-full"
-          src="/Logos/brninsaat.jpeg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          class="sm:h-[200px] md:h-[230px] object-cover w-full"
-          src="/Logos/mobilyasitesi.jpeg"
-          alt=""
+      <swiper-slide v-for="reference in references" :key="reference"
+        ><img class="max-w-[300px] object-cover w-full" :src="reference" alt=""
       /></swiper-slide>
     </swiper>
   </div>
@@ -60,16 +45,54 @@ import "swiper/css/pagination";
 
 // import required modules
 import { FreeMode, Pagination, Autoplay } from "swiper/modules";
+import { getStorage, ref, getDownloadURL, listAll } from "firebase/storage";
 
 export default {
   components: {
     Swiper,
     SwiperSlide,
   },
+  data() {
+    return {
+      references: [],
+    };
+  },
   setup() {
     return {
       modules: [FreeMode, Pagination, Autoplay],
     };
+  },
+
+  async mounted() {
+    const storage = getStorage();
+    // Get Product Images
+    const imagesRef = ref(storage, `references`);
+    try {
+      const res = await listAll(imagesRef);
+      await Promise.all(
+        res.items.map(async (itemRef) => {
+          const url = await getDownloadURL(itemRef);
+          this.references.push(url);
+        })
+      );
+    } catch (error) {
+      switch (error.code) {
+        case "storage/object-not-found":
+          console.log("File doesn't exist");
+          break;
+        case "storage/unauthorized":
+          console.log("User doesn't have permission to access the object");
+          break;
+        case "storage/canceled":
+          console.log("User canceled the upload");
+          break;
+        case "storage/unknown":
+          console.log("Unknown error occurred, inspect the server response");
+          break;
+        default:
+          console.log(error);
+      }
+    }
   },
 };
 </script>
