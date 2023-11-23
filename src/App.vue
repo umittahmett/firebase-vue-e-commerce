c2061332e079b04f0344f254c9649b1b5147e63d
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-11-23 23:54:07 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="w-full">
-    <Navbar />
     <router-view />
-    <Footer />
   </div>
 </template>
-
-<script>
-import Navbar from "../src/components/Menu/Navbar.vue";
-import Footer from "../src/components/Footer.vue";
-
-export default {
-  data() {
-    return {};
-  },
-  components: {
-    Navbar,
-    Footer,
-  },
-};
-</script>
