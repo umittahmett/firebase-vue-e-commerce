87ef6b2d35e19cd4a11b3bbf0cc6ed73d8ccbaca
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-11-27 11:02:10 +0300

    Change Texts of 404Page

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,17 +1,21 @@
 <template>
+  <!-- 404 Page -->
   <main
     class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 h-[80vh]"
   >
     <div class="text-center">
       <p class="text-base font-semibold text-blue-500">404</p>
+
       <h1
         class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl"
       >
-        Page not found
+        Sayfa bulunamadı
       </h1>
+
       <p class="mt-6 text-base leading-7 text-gray-600">
-        Sorry, we couldn’t find the page you’re looking for.
+        Üzgünüz, aradığınız sayfayı bulamadık.
       </p>
+
       <div class="mt-10 flex items-center justify-center gap-x-6">
         <a
           href="/"
