5d437c6fef3ae05e51f26c8f16b440c8664f9480
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-02 20:39:13 +0300

    Create Message Component

---
+++ src/components/Common/Message.vue
@@ -0,0 +1,45 @@
+<!-- Feedback Message -->
+<template>
+  <div class="fixed top-12 right-3 max-w-[400px]">
+    <Message
+      v-if="store && store.feedBackMessageVisible"
+      :severity="messageType"
+      >{{ text }}</Message
+    >
+  </div>
+</template>
+
+<script>
+import Message from "primevue/message";
+import { createWizardStore } from "../../stores/counter";
+
+export default {
+  components: {
+    Message,
+  },
+
+  data() {
+    return {
+      store: null,
+    };
+  },
+  props: {
+    text: {
+      type: String,
+      default: null,
+    },
+    messageType: {
+      type: String,
+      default: "",
+    },
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  mounted() {
+    this.store = createWizardStore();
+  },
+};
+</script>
